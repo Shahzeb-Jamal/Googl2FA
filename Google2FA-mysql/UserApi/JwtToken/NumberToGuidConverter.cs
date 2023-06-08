25df429d3e7c103d3939207cfe31917941a9510c
Shahzeb-Jamal <jamalshahzeb@gmail.com>
2023-06-08 11:30:11 +0530

    Comment un-used code

--- Google2FA-mysql/UserApi/JwtToken/NumberToGuidConverter.cs
+++ Google2FA-mysql/UserApi/JwtToken/NumberToGuidConverter.cs
@@ -1,21 +1,21 @@
-﻿using System.Security.Cryptography;
+﻿//using System.Security.Cryptography;
 
-namespace UserApi.JwtToken
-{
-    public static class NumberToGuidConverter
-    {
-        public static Guid NumberToGuid(int number)
-        {
-            byte[] numberBytes = BitConverter.GetBytes(number);
+//namespace UserApi.JwtToken
+//{
+//    public static class NumberToGuidConverter
+//    {
+//        public static Guid NumberToGuid(int number)
+//        {
+//            byte[] numberBytes = BitConverter.GetBytes(number);
 
-            using (var sha1 = SHA1.Create())
-            {
-                byte[] hashedBytes = sha1.ComputeHash(numberBytes);
-                Array.Resize(ref hashedBytes, 16); // Keep only the first 16 bytes
+//            using (var sha1 = SHA1.Create())
+//            {
+//                byte[] hashedBytes = sha1.ComputeHash(numberBytes);
+//                Array.Resize(ref hashedBytes, 16); // Keep only the first 16 bytes
 
-                return new Guid(hashedBytes);
-            }
-        }
-    }
+//                return new Guid(hashedBytes);
+//            }
+//        }
+//    }
 
-}
+//}
