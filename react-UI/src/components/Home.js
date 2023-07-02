be51f9add652ab1e67c4ac5c58a76b28d3323269
Shahzeb-Jamal <jamalshahzeb@gmail.com>
2023-07-02 19:38:26 +0530

    Added css to components

--- react-UI/src/components/Home.js
+++ react-UI/src/components/Home.js
@@ -1,113 +1,87 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState} from 'react';
+import { useLocation } from 'react-router-dom';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
-
-
+import "../components/style/Home.css";
 
 const Home = () => {
-  const navigate = useNavigate();
-  const [captchaImage, setCaptchaImage] = useState('');
-  const [captchaText, setCaptchaText] = useState('');
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [validationMessage, setValidationMessage] = useState('');
+  const location = useLocation();
+  const { username } = location.state;
+  const [qrCodeSetupImageUrl, setQRCodeSetupImageUrl] = useState('');
+  const [authenticationPin, setAuthenticationPin] = useState('');
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [error, setError] = useState('');
 
   useEffect(() => {
-    fetchCaptchaImage();
-  }, []);
-
-  const fetchCaptchaImage = async () => {
-    try {
-      const response = await axios.get('https://localhost:7114/api/Captcha/GetCaptcha?timestamp=${timestamp}');
-      const { captchaImage } = response.data;
-      setCaptchaImage(captchaImage);
-    } catch (error) {
-      console.error('Error fetching captcha:', error);
-    }
-  };
-  
-
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-  
-    try {
-      const response = await axios.post('https://localhost:7114/api/Captcha/ValidateCaptcha', { captcha: captchaText });
-      const { isValid } = response.data;
-      
-      console.log(isValid);
-      if (isValid) {
-        setValidationMessage('Captcha validation successful');
-        getToken();
-      } else {
-        setValidationMessage('Invalid captcha');
-        alert('Invalid captcha'); // Show alert for invalid captcha
-        setCaptchaText(''); // Clear the captcha text input
-        fetchCaptchaImage(); // Reload the captcha image
-      }
-    } catch (error) {
-      console.error('Error validating captcha:', error);
-    }
-  };
-  
-  
-
-  const getToken = () => {
+    const token = localStorage.getItem(username);
+    console.log('Contact-Token:', token);
     axios
-      .post('https://localhost:7114/api/Users/token', {
-        username,
-        password,
-        captcha: captchaText,
+      .get('https://localhost:7114/api/Users', {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
       })
       .then((response) => {
-        const { token } = response.data;
-        localStorage.setItem(username, token);
-        console.log('Token:', token);
-        navigate('/contact', { state: { username } }); // Pass the username as state when navigating to Contact
+        console.log(response.data);
+        console.log(response.data.qrCodeSetupImageUrl);
+      setQRCodeSetupImageUrl(response.data.qrCodeSetupImageUrl);
+        console.log(qrCodeSetupImageUrl);
       })
       .catch((error) => {
-        console.error('Error getting token:', error);
+        console.error('Error fetching QR code:', error);
+      });
+  }, []);
+
+  const handlePinChange = (e) => {
+   setAuthenticationPin(e.target.value);
+  };
+
+  const handlePinSubmit = (e) => {
+    e.preventDefault();
+    const token = localStorage.getItem(username);
+    axios
+      .post(
+        'https://localhost:7114/api/Users/Validate2FACode',
+        { Google2FACode: authenticationPin },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      )
+      .then((response) => {
+        console.log(response.data);
+       setIsAuthenticated(true);
+       setError('');
+      })
+      .catch((error) => {
+        console.error('Error validating 2FA code:', error);
+       setIsAuthenticated(false);
+       setError('Google Two Factor PIN is expired or wrong!');
+       resetPIN();
       });
   };
 
+  const resetPIN = () => {
+    setAuthenticationPin('');
+}
+
   return (
-    <div>
-      <h1>Home</h1>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="username">Username:</label>
-          <input
-            type="text"
-            id="username"
-            value={username}
-            onChange={(event) => setUsername(event.target.value)}
-          />
-        </div>
-        <div>
-          <label htmlFor="password">Password:</label>
-          <input
-            type="password"
-            id="password"
-            value={password}
-            onChange={(event) => setPassword(event.target.value)}
-          />
-        </div>
-        <div>
-          <img src={`data:image/png;base64,${captchaImage}`} alt="Captcha" />
-        </div>
-        <div>
-          <label htmlFor="captchaText">Enter Captcha:</label>
-          <input
-            type="text"
-            id="captchaText"
-            value={captchaText}
-            onChange={(event) => setCaptchaText(event.target.value)}
-          />
-        </div>
-        <div>
-          <button type="submit">Submit</button>
-        </div>
-      </form>
-      {validationMessage && <p>{validationMessage}</p>}
+    <div className='container'>
+      {isAuthenticated ? (
+        <h1>Welcome {username}</h1>
+      ) : (
+        <>
+          <img src={`${qrCodeSetupImageUrl}`} alt="Google 2FA QR Code" width="300" height="300" />
+          {!isAuthenticated && (
+            <form onSubmit={handlePinSubmit}>
+              <input type="text" value={authenticationPin} onChange={handlePinChange} placeholder="Enter PIN" />
+              <button type="submit">Submit</button>
+            </form>
+          )}
+          {error && <p>{error}</p>}
+        </>
+      )}
     </div>
   );
 };
